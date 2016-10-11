0e573156d6345603ee6ff2ededbd053752b35777
twarlis <info@sevenimagegroup.com>
2016-10-11 12:48:54 -0400

    Added Files from Dropbox

---
+++ 21743_Denfield_Road_-_Home_files/21743_Denfield_Road_-_Home.js
@@ -0,0 +1,7 @@
+// Created by iWeb 2.0 local-build-20161006
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('21743_Denfield_Road_-_Home_files/21743_Denfield_Road_-_HomeMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
---
+++ 21743_Denfield_Road_-_Web_Tour_files/21743_Denfield_Road_-_Web_Tour.js
@@ -0,0 +1,7 @@
+// Created by iWeb 2.0 local-build-20161006
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('21743_Denfield_Road_-_Web_Tour_files/21743_Denfield_Road_-_Web_TourMoz.css')
+fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
